94c8acee0bbd86c366816b1e4e22042192736015
gaokai <gaokai.zh@qq.com>
2020-06-05 16:30:09 +0800

    init commit

--- dubbo-redis-cache-core/src/main/java/com/github/dubbo/cache/el/CacheKeyBySpELGenerator.java
+++ dubbo-redis-cache-core/src/main/java/com/github/dubbo/cache/el/CacheKeyBySpELGenerator.java
@@ -2,25 +2,26 @@ package com.github.dubbo.cache.el;
 
 import com.github.dubbo.cache.CacheKeyGenerator;
 import com.github.dubbo.cache.CacheMetadata;
+import org.apache.dubbo.common.logger.Logger;
+import org.apache.dubbo.common.logger.LoggerFactory;
 import org.apache.dubbo.common.utils.StringUtils;
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.expression.Expression;
-import org.springframework.expression.common.TemplateParserContext;
 import org.springframework.expression.spel.SpelCompilerMode;
 import org.springframework.expression.spel.SpelParserConfiguration;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * spring el key生成器
+ * spring SpEL key生成器
  */
 public class CacheKeyBySpELGenerator implements CacheKeyGenerator {
+
+    private static final Logger logger = LoggerFactory.getLogger(CacheKeyBySpELGenerator.class);
 
     private final SpelExpressionParser spelExpressionParser;
     private final ParameterNameDiscoverer parameterNameDiscoverer;
@@ -35,18 +36,23 @@ public class CacheKeyBySpELGenerator implements CacheKeyGenerator {
 
     @Override
     public Object key(CacheMetadata cacheMetadata, Object[] args) {
-        if (StringUtils.isBlank(cacheMetadata.getKey())) {
-            return null;
+        try {
+            if (StringUtils.isBlank(cacheMetadata.getKey())) {
+                return null;
+            }
+            ExpressionValueHolder expressionValueHolder = cache.get(cacheMetadata.getMethod());
+            if (expressionValueHolder == null) {
+                Expression expression = spelExpressionParser.parseExpression(cacheMetadata.getKey());
+                String[] parameterNames = parameterNameDiscoverer.getParameterNames(cacheMetadata.getMethod());
+                expressionValueHolder = new ExpressionValueHolder(expression, parameterNames);
+                cache.putIfAbsent(cacheMetadata.getMethod(), expressionValueHolder);
+            }
+            CacheEvaluationContext context = new CacheEvaluationContext(cacheMetadata, cacheMetadata.getMethod(), args, expressionValueHolder.parameterNames);
+            return expressionValueHolder.expression.getValue(context);
+        } catch (Exception e) {
+            logger.warn("spring el parse failure", e);
         }
-        ExpressionValueHolder expressionValueHolder = cache.get(cacheMetadata.getMethod());
-        if (expressionValueHolder == null) {
-            Expression expression = spelExpressionParser.parseExpression(cacheMetadata.getKey(), new TemplateParserContext());
-            String[] parameterNames = parameterNameDiscoverer.getParameterNames(cacheMetadata.getMethod());
-            expressionValueHolder = new ExpressionValueHolder(expression, parameterNames);
-            cache.putIfAbsent(cacheMetadata.getMethod(), expressionValueHolder);
-        }
-        CacheEvaluationContext context = new CacheEvaluationContext(cacheMetadata, cacheMetadata.getMethod(), args, expressionValueHolder.parameterNames);
-        return expressionValueHolder.expression.getValue(context);
+        return null;
     }
 
     private static class ExpressionValueHolder {
@@ -57,23 +63,6 @@ public class CacheKeyBySpELGenerator implements CacheKeyGenerator {
         public ExpressionValueHolder(Expression expression, String[] parameterNames) {
             this.expression = expression;
             this.parameterNames = parameterNames;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            ExpressionValueHolder that = (ExpressionValueHolder) o;
-            return expression.equals(that.expression) && Arrays.equals(parameterNames, that.parameterNames);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(expression, parameterNames);
         }
     }
 }
